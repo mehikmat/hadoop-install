b30e51eac4d01e9bd6feb87e40f75fd62f93cb36
Hikmat Dhamee <mehikmat@users.noreply.github.com>
2014-05-29 20:56:23 -0700

    add hadoop stop script

---
+++ stop-hadoop.sh
@@ -0,0 +1,8 @@
+#Start all hadoop daemons
+$HADOOP_HOME/sbin/stop-dfs.sh
+$HADOOP_HOME/sbin/stop-yarn.sh
+$HADOOP_HOME/sbin/mr-jobhistory-daemon.sh stop historyserver
+$HADOOP_HOME/sbin/yarn-daemon.sh stop proxyserver
+
+#Show daemons
+$JAVA_HOME/bin/jps
